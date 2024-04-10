6378c0661db1e4580df19f1f24fd9ce4cec239e5
Mirzhakyp <zhakiyamir@gmail.com>
2024-04-10 19:35:49 +0500

    without docker

---
+++ src/main/java/com/example/rockpaperscissorwebsocket/RockpaperscissorWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.rockpaperscissorwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockpaperscissorWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockpaperscissorWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rockpaperscissorwebsocket/RockpaperscissorWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rockpaperscissorwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockpaperscissorWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
